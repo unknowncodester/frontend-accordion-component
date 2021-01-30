2317a71ec6dddcb58955564ab4c53321dbc0d545
Dan <sgdhardy1991@gmail.com>
2021-01-30 02:12:01 +0000

    Add initial faq card

---
+++ README.md
@@ -0,0 +1,7 @@
+# Profile card component
+
+![Design preview for the Faq accordion card component coding challenge](./design/desktop-preview.jpg)
+
+## What is the repository for?
+
+This repository is based off the front end coding challenges from the website [Frontend Mentor](https://www.frontendmentor.io). 
---
+++ accordion.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+  let questions = document.querySelectorAll(".card__question");
+
+  questions.forEach(function(question) {
+      question.addEventListener("click", function() {
+        question.parentElement.children[1].classList.toggle("card__answer--hide");        
+      });
+  });
+});
