d9f730001b9d823932fc65d4669f126833c61d3a
Dan <sgdhardy1991@gmail.com>
2021-02-10 21:38:45 +0000

    Reposition chevron icon, add toggle code

--- accordion.js
+++ accordion.js
@@ -1,12 +1,46 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-  
-  let questions = document.querySelectorAll(".card__question");
+document.addEventListener("DOMContentLoaded", function () {
+    const questions = document.querySelectorAll(".card__question");
 
-  questions.forEach(function(question) {
-      question.addEventListener("click", function() {
-          question.parentElement.children[1].classList.toggle("card__answer--hide");  
-          question.classList.toggle("card__question--flipped");              
-      });
-  });
+    questions.forEach((question) => {
+        question.addEventListener("click", function () {
+            let targetIcon = question.children[1];
+            let targetAnswer = question.nextElementSibling;
+            
+            const visibleAnswer = document.querySelector("p:not(.question__answer--hide)");
+            const flippedIcon = document.querySelector(".question__icon--flipped");
+
+            if(visibleAnswer !== targetAnswer) {
+                toggleAnswerVisibility(visibleAnswer);
+                rotateIcon(flippedIcon);
+            }
+            
+            toggleAnswerVisibility(targetAnswer);
+            rotateIcon(targetIcon);
+        });
+    });
+
+    /**
+     * Toggle whether the clicked answer should be displayed
+     * 
+     * @param {Element} answer 
+     * @return void
+     */
+    function toggleAnswerVisibility(answer) {
+        if(answer){
+            answer.classList.toggle("question__answer--hide");
+        }
+    }
+
+    /**
+     * Rotate the question icon
+     * 
+     * @param {Element} icon
+     * @return void
+     */
+    function rotateIcon(icon) {
+        if(icon) {
+            icon.classList.toggle("question__icon--flipped");            
+        }
+    }
 });
