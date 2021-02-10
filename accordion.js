8414fcc277d0e9e69bb8b5083006b65d804c5982
Dan <sgdhardy1991@gmail.com>
2021-02-10 23:02:18 +0000

    Update faq spacing, line height, font sizes, colors, etc

--- accordion.js
+++ accordion.js
@@ -4,19 +4,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     questions.forEach((question) => {
         question.addEventListener("click", function () {
-            let targetIcon = question.children[1];
-            let targetAnswer = question.nextElementSibling;
-            
-            const visibleAnswer = document.querySelector("p:not(.question__answer--hide)");
-            const flippedIcon = document.querySelector(".question__icon--flipped");
+            const targetIcon = question.children[1];
+            const flippedIcon = document.querySelector(".question__icon--flipped");         
+            const targetAnswer = question.nextElementSibling;
+            const visibleAnswer = document.querySelector("p:not(.question__answer--hide)");   
+            const targetQuestionTitle = question.children[0];
+            const activeQuestionTitle = document.querySelector(".question__title--active");
 
             if(visibleAnswer !== targetAnswer) {
                 toggleAnswerVisibility(visibleAnswer);
                 rotateIcon(flippedIcon);
+                toggleQuestionActive(activeQuestionTitle);
             }
             
             toggleAnswerVisibility(targetAnswer);
             rotateIcon(targetIcon);
+            toggleQuestionActive(targetQuestionTitle);
         });
     });
 
@@ -41,6 +44,17 @@ document.addEventListener("DOMContentLoaded", function () {
     function rotateIcon(icon) {
         if(icon) {
             icon.classList.toggle("question__icon--flipped");            
+        }
+    }
+
+    /**
+     * Toggle whether the question is active
+     * 
+     * @param {Element} question 
+     */
+    function toggleQuestionActive(question){
+        if(question) {
+            question.classList.toggle("question__title--active");            
         }
     }
 });
